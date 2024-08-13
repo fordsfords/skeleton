b785fd3b577f8d01e7b7b4588f06d19f2227b3f3
Steve Ford <sford@geeky-boy.com>
2024-08-13 11:48:47 -0400

    updates

--- skeleton.py
+++ skeleton.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-skeleton.py - Skeletal example.
+skeleton2.py - Skeletal example.
 """
 
 import sys
@@ -9,46 +9,54 @@ import fileinput
 import re
 
 
-def process_line(line):
+class Main:
     """
-    Skeletal function.
+    Main program.
     """
 
-    if re.search(r'^quit$', line):
-        sys.exit(0)
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Demo script to show when argparse displays help.",
+            epilog="This text appears at the end of the help message."
+        )
+        parser.add_argument('-n', '--name', help="Name")
+        parser.add_argument('-i', '--id', type=int, help="Numeric ID")
+        parser.add_argument('-q', '--quiet', action='store_true',
+                            help="suppress informational messages.")
+        parser.add_argument('files', nargs='*',
+                            help="Input files (use - for stdin)")
 
-    match = re.search(r'^echo (.*)$', line)
-    if match:
-        print(match.group(1))
-        return
+        self.args = parser.parse_args()
 
-    print(f'Error [{fileinput.filename()}:{fileinput.filelineno()}]'
-          f' - bad command "{line}"')
+    def process_line(self, line) -> None:
+        """
+        Skeletal function.
+        """
 
+        if re.search(r'^quit$', line):
+            sys.exit(0)
 
-def main() -> None:
-    """
-    Skeletal main.
-    """
-    parser = argparse.ArgumentParser(
-        description="Demo script to show when argparse displays help.",
-        epilog="This text appears at the end of the help message."
-    )
-    parser.add_argument('-n', '--name', help="Name")
-    parser.add_argument('-i', '--id', type=int, help="Numeric ID")
-    parser.add_argument('files', nargs='*',
-                        help="Input files (use - for stdin)")
+        match = re.search(r'^echo (.*)$', line)
+        if match:
+            if not self.args.quiet:
+                print(match.group(1))
+            return
 
-    args = parser.parse_args()
+        print(f'Error [{fileinput.filename()}:{fileinput.filelineno()}]'
+              f' - bad command "{line}"')
 
-    print(f"Name: {args.name}")
-    print(f"ID: {args.id}")
+    def main(self):
+        """ Main. """
 
-    # Similar to Perl's 'while (<>) {' diamond operator.
-    with fileinput.input(files=args.files) as file_input:
-        for line in file_input:
-            process_line(line.strip())
+        if not self.args.quiet:
+            for arg, value in vars(self.args).items():
+                print(f"{arg}: {value}")
+
+        # Similar to Perl's 'while (<>) {' diamond operator.
+        with fileinput.input(files=self.args.files) as file_input:
+            for line in file_input:
+                self.process_line(line.strip())
 
 
 if __name__ == "__main__":
-    main()
+    Main().main()
